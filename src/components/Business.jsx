ec3ffb87563ccb9b9d6f96e2b4b040d668f2ac96
Hamzat-Ridwan <ridwanhamzat99@gmail.com>
2022-10-31 22:20:11 +0100

    Remove flex for tablets

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -5,7 +5,7 @@ import FeatureCard from './FeatureCard'
 
 const Business = () => {
   return (
-    <div className='text-white sm:flex justify-between'>
+    <div className='text-white md:flex justify-between'>
         <div className='mb-[40px]'>
             <h1 className='font-semibold text-5xl font-poppins'>You do the business, <br/>we’ll handle the money.</h1>
             <p className='sm:w-[60%] font-poppins pt-[20px] pb-[40px] text-base font-light'>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
